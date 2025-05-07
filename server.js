e1c762233e2415c557c4786b047f624d9933fd21
z-tech-learn-2025 <z.tech.learn.2025@gmail.com>
2025-05-07 07:37:22 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,100 @@
+// Needed for dotenv
+require("dotenv").config();
+
+// Needed for Express
+var express = require('express')
+var app = express()
+
+// Needed for EJS
+app.set('view engine', 'ejs');
+
+// Needed for public directory
+app.use(express.static(__dirname + '/public'));
+
+// Needed for parsing form data
+app.use(express.json());       
+app.use(express.urlencoded({extended: true}));
+
+// Needed for Prisma to connect to database
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient();
+
+// Main landing page
+app.get('/', async function(req, res) {
+
+    // Try-Catch for any errors
+    try {
+        // Get all blog posts
+        const blogs = await prisma.post.findMany({
+                orderBy: [
+                  {
+                    id: 'desc'
+                  }
+                ]
+        });
+
+        // Render the homepage with all the blog posts
+        await res.render('pages/home', { blogs: blogs });
+      } catch (error) {
+        res.render('pages/home');
+        console.log(error);
+      } 
+});
+
+// About page
+app.get('/about', function(req, res) {
+    res.render('pages/about');
+});
+
+// New post page
+app.get('/new', function(req, res) {
+    res.render('pages/new');
+});
+
+// Create a new post
+app.post('/new', async function(req, res) {
+    
+    // Try-Catch for any errors
+    try {
+        // Get the title and content from submitted form
+        const { title, content } = req.body;
+
+        // Reload page if empty title or content
+        if (!title || !content) {
+            console.log("Unable to create new post, no title or content");
+            res.render('pages/new');
+        } else {
+            // Create post and store in database
+            const blog = await prisma.post.create({
+                data: { title, content },
+            });
+
+            // Redirect back to the homepage
+            res.redirect('/');
+        }
+      } catch (error) {
+        console.log(error);
+        res.render('pages/new');
+      }
+
+});
+
+// Delete a post by id
+app.post("/delete/:id", async (req, res) => {
+    const { id } = req.params;
+    
+    try {
+        await prisma.post.delete({
+            where: { id: parseInt(id) },
+        });
+      
+        // Redirect back to the homepage
+        res.redirect('/');
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+  });
+
+// Tells the app which port to run on
+app.listen(8080);
